f722d24aca2f6907c3a10b7bb5c24012f3938a92
Dakota <dakotasankey@gmail.com>
2018-08-25 16:42:49 -0700

    cut literally 40 lines out of #validates and made it faster
    marin pls

--- scripts/other_utilities.rb
+++ scripts/other_utilities.rb
@@ -7,52 +7,17 @@ class Numeric
   end
 end
 
-def validate(hash)
-  for key in hash.keys
-    v = hash[key]
-    v = [v] unless v.is_a?(Array)
-    match = false
-    v.each do |e|
-      break if match
-      if e.is_a?(Symbol)
-        match = true if key.respond_to?(e)
-      elsif e.is_a?(Class) || e.is_a?(Module)
-        match = true if key.is_a?(e)
-      end
-    end
-    unless match
-      types = v.select { |e| e.is_a?(Class) || e.is_a?(Module) }
-      meths = v.select { |e| e.is_a?(Symbol) }
-      errmsg = "Invalid argument passed to method.\n\nType: #{key.class}\n"
-      if types.size > 0
-        typestr = ""
-        if types.size == 1
-          typestr = types[0].to_s
-        elsif types.size == 2
-          typestr = types[0].to_s + " or " + types[1].to_s
-        else
-          for i in 0...types.size - 1
-            typestr << types[i].to_s + ", "
-          end
-          typestr << "or " + types.last.to_s
-        end
-        errmsg << "Expected class: #{typestr}\n"
-      end
-      if meths.size > 0
-        methstr = ""
-        if meths.size == 1
-          methstr = meths[0].to_s
-        elsif meths.size == 2
-          methstr = meths[0].to_s + " or " + meths[1].to_s
-        else
-          for i in 0...meths.size - 1
-            methstr << meths[i].to_s + ", "
-          end
-          methstr << "or " + meths.last.to_s
-        end
-        errmsg << "Expected method support: #{methstr}\n"
-      end
-      raise ArgumentError, errmsg
+def validates(hash)
+  errors = hash.map do |key, value|
+    if value.is_a?(Array)
+      "Expected #{key} to be one of [#{value.join(",")}], got #{key.class}" unless value.include?(key.class)
+    elsif value.is_a?(Symbol)
+      "Expected #{key} to respond_to #{value}" unless key.respond_to?(value)
+    else
+      "Expected #{key} to be a #{value}, got #{key.class}" unless key.is_a?(value)
     end
   end
+
+  return if errors.none?
+  raise ArgumentError, errors.compact.join(", ")
 end
